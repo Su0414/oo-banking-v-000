ce5f37d9011586e3878938a9b9437d3c4f4c753b
Suchitra Naik <susanaik@gmail.com>
2018-05-07 13:53:14 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,7 +2,7 @@ class BankAccount
   
   attr_accessor :name ,:balance
   
-  def initialize(name, balance = 1000, status = "OPEN")
+  def initialize(name, balance = 1000, status = "open")
     @name = name 
   end
   
