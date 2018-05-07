eb0a7e74485232a4901a381b85ac7c161ba5ed71
Suchitra Naik <susanaik@gmail.com>
2018-05-07 14:55:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -36,7 +36,7 @@ class Transfer
   end 
   
   def reverse_transfer 
-    if(@status != "reversed")
+    if(@status == "complete" || @status != "reversed")
         @sender.balance = @sender.balance + @amount
         @receiver.balance = @receiver.balance - @amount
         @status = "reversed"
