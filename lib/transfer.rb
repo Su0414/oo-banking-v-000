6341fa9f5c9de5cc64c7cfa9f790e4215f1e04e3
Suchitra Naik <susanaik@gmail.com>
2018-05-07 14:46:31 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,8 @@ class Transfer
   end 
   
   def execute_transaction 
-    if(@sender.valid? && @status != "complete")
+    if(@sender.valid? && @status != "complete" && @sender.balance > @amount)
+      
         @sender.balance = @sender.balance - @amount
         @receiver.balance = @receiver.balance + @amount
         @status = "complete"
