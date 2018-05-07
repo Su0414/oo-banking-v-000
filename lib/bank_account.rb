545bb1d6552141ab5ee74104984b1cb8a7a935ad
Suchitra Naik <susanaik@gmail.com>
2018-05-07 13:52:18 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,9 @@
 class BankAccount
   
-  attr_accessor :name 
+  attr_accessor :name ,:balance
   
-  def initialize(name)
+  def initialize(name, balance = 1000)
     @name = name 
-
+  end
+  
 end
