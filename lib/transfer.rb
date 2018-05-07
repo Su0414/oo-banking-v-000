acf05a884f796bf61cc2320a591de48dde1e760c
Suchitra Naik <susanaik@gmail.com>
2018-05-07 14:42:00 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,15 +22,15 @@ class Transfer
   end 
   
   def execute_transaction 
-    if  !@sender.valid?
-      "Transaction rejected. Please check your account balance."
-      @status = "rejected"
-    else 
-      if(@status != "complete")
+    if valid? && @status != "complete")
         @sender.balance = @sender.balance - @amount
         @receiver.balance = @receiver.balance + @amount
         @status = "complete"
       end 
+    else 
+      "Transaction rejected. Please check your account balance."
+      @status = "rejected"
+    
     end
   end 
   
